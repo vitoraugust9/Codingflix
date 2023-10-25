25dbb56134badfd1c50ae7f063e8fb41928037ed
vitor.sfernandes@linx.com.br <vitor.sfernandes@linx.com.br>
2023-10-25 17:37:50 -0300

    Criação dos elementos da página de informações do usuário, do seu estilo visual e também da sua responsividade completa - profile (Usuário)

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/profile.module.scss";
 import UserForm from "@/src/components/profile/user";
+import HeaderAuth from "@/src/components/common/headerAuth";
+import { Button, Col, Container, Row } from "reactstrap";
+import Footer from "@/src/components/common/footer";
 
 const UserInfo = function () {
   return (
@@ -10,7 +13,28 @@ const UserInfo = function () {
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <main>
-        <UserForm/>
+      <div className={styles.header}> 
+	      <HeaderAuth />
+      </div>
+          <Container className="py-5">
+            <p className={styles.title}>Minha Conta</p>
+            <Row className="pt-3 pb-5">
+              <Col md={4} className={styles.btnColumn}>
+                <Button outline className={styles.renderFormBtn}>
+                  DADOS PESSOAIS
+                </Button>
+                <Button outline className={styles.renderFormBtn}>
+                  SENHA
+                </Button>
+              </Col>
+              <Col md>
+                <UserForm />
+              </Col>
+            </Row>
+          </Container>
+          <div className={styles.footer}>
+            <Footer />
+          </div>
       </main>
     </>
   );

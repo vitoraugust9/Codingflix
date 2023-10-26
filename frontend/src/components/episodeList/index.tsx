4dc663d759029dc6a01e3d33ee71cd858dbd4db0
vitor.sfernandes@linx.com.br <vitor.sfernandes@linx.com.br>
2023-10-26 13:30:04 -0300

    Criação dos elementos da página de espisódio, do seu estilo visual e também da sua responsividade completa - Episódio

--- frontend/src/components/episodeList/index.tsx
+++ frontend/src/components/episodeList/index.tsx
@@ -1,36 +1,46 @@
 import styles from "./styles.module.scss";
-import { EpisodeType } from "../../services/courseService";
+import { CourseType, EpisodeType } from "../../services/courseService";
+import { useRouter } from "next/router";
 
 interface props {
   episode: EpisodeType;
+  course: CourseType;
 }
 
-const EpisodeList = function ({ episode }: props) {
-    const handleSecondsToMin = (totalSeconds: number) => {
-        const minutes = Math.floor(totalSeconds / 60);
-    
-        const seconds = totalSeconds % 60;
-    
-        function toString(num: number) {
-          return num.toString().padStart(2, "0");
-        }
-    
-        const result = `${toString(minutes)}:${toString(seconds)}`;
-    
-        return result;
-      };
+const EpisodeList = function ({ episode, course }: props) {
+  const router = useRouter();
+  const handleSecondsToMin = (totalSeconds: number) => {
+    const minutes = Math.floor(totalSeconds / 60);
+
+    const seconds = totalSeconds % 60;
+
+    function toString(num: number) {
+      return num.toString().padStart(2, "0");
+    }
+
+    const result = `${toString(minutes)}:${toString(seconds)}`;
+
+    return result;
+  };
+  const handleEpisodePlayer = () => {
+    router.push(
+      `/courses/episode/${episode.order - 1}?courseid=${course.id}`
+    );
+  };
+
+
   return <>
-  <div className={styles.episodeCard}>
-    <div className={styles.episodeOrderTime}>
-      <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
-    <p className={styles.episodeTime}>{handleSecondsToMin(episode.secondsLong)}</p>
-  </div>
-  <div className={styles.episodeTitleDescription}>
-      <p className={styles.episodeTitle}>{episode.name}</p>
-    <p className={styles.episodeDescription}>{episode.synopsis}</p>
-  </div>
-</div>
-</>;
+    <div className={styles.episodeCard} onClick={handleEpisodePlayer}>
+      <div className={styles.episodeOrderTime}>
+        <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
+        <p className={styles.episodeTime}>{handleSecondsToMin(episode.secondsLong)}</p>
+      </div>
+      <div className={styles.episodeTitleDescription}>
+        <p className={styles.episodeTitle}>{episode.name}</p>
+        <p className={styles.episodeDescription}>{episode.synopsis}</p>
+      </div>
+    </div>
+  </>;
 };
 
 export default EpisodeList;
